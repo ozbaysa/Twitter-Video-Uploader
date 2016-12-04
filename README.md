36db09b44eb90feac565173754e3ed167f1b92ac
ozbaysa <sadikekinozbay@gmail.com>
2016-12-04 22:53:24 +0300

    Files are added Enjoy

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Twitter-Video-Uploader-
-This is a video upload program for twitter
+Hello guys, When i was trying to upload video to twitter with Twitter Gem, i had really bad times. It was incredibly hard. So i decided to write video upload program for twitter.
+
+How to use this ruby file
+
+1- Put this(twitter_video.rb) to somewhere in your project that you can reach easly.
+
+2- In your code blocks call the file  ```require_relative 'twitter_account.rb'```
+
+3- Initialize the file ```TwitterVideoUploader.new(video_path, account_username, account_password, consumer_key, secret_key, status_message).upload```
+
+3.5 Or you can do this ```twitter_video = TwitterVideoUploader.new(video_path, account_username, account_password, consumer_key, secret_key, status_message)
+twitter_video.upload
+```
+
+4- And the most important one is Enjoy :kissing_heart:
+
+
+
+###### Made with :kissing_heart: in Istanbul
---
+++ twitter_video.rb
@@ -0,0 +1,45 @@
+require 'watir-webdriver'
+require 'open3'
+require 'json'
+require 'colorize'
+
+class TwitterVideoUploader
+
+  def initialize(video_path, username, password, consumer_key, secret_key, status_message)
+    @video_path = video_path
+    @username = username
+    @password = password
+    @consumer_key = consumer_key
+    @secret_key = secret_key
+    @status_message = status_message
+  end
+
+  def upload
+    stdin, stdout = Open3.popen3("twurl authorize -u #{@username} -p #{@password} --consumer-key #{@consumer_key} --consumer-secret #{@secret_key}")
+    line = stdout.gets
+    x = 6
+    url = ' '
+    while line[x] != ' '
+      url += line[x]
+      x += 1
+    end
+    puts 'Opening the browser for validation'.green
+    b = Watir::Browser.new :chrome
+    b.goto url
+    b.text_field(id: 'username_or_email').set @username
+    b.text_field(id: 'password').set @password
+    b.button(id: 'allow').click
+    code = b.code.text
+    stdin.puts(code)
+    puts 'Validation is done!'.green
+    b.close
+    puts 'Loading the file...'.green
+    output = `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=INIT&media_type=video/mp4&total_bytes=#{File.new(@video_path).size}"`
+    media_id = JSON.parse(output)['media_id']
+    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=APPEND&media_id=#{media_id}&segment_index=0" --file #{@video_path}  --file-field "media"`
+    `twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=FINALIZE&media_id=#{media_id}"`
+    `twurl -d 'status=#{@status_message}&media_ids=#{media_id}' /1.1/statuses/update.json`
+    puts 'Everything is done enjoy 😘'.green
+    puts 'Bye 👋'.blue
+  end
+end
